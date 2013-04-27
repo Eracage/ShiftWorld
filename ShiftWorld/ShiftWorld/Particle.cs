ddff7694ab6b21e782cde00cdfd1bb8c7ccfa601
Esko Haila <eracage@gmail.com>
2013-04-27 20:00:23 +0300

    added code to particles

--- ShiftWorld/ShiftWorld/Particle.cs
+++ ShiftWorld/ShiftWorld/Particle.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using FuncWorks.XNA.XTiled;
+using System.Diagnostics;
+using System.Media;
 using System.Text;
 
 namespace ShiftWorld
 {
     class Particle
     {
+        Texture2D _texture;
+        float _life;
+
+        public Particle(Texture2D texture)
+        {
+            _texture = texture;
+            _life = 500;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _life -= (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+        }
+
+        public float Life
+        {
+            get { return _life; }
+        }
+
+        public void Draw()
+        {
+
+        }
     }
 }
--- ShiftWorld/ShiftWorld/ParticleController.cs
+++ ShiftWorld/ShiftWorld/ParticleController.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using FuncWorks.XNA.XTiled;
+using System.Diagnostics;
+using System.Media;
 using System.Text;
 
 namespace ShiftWorld
 {
+
     class ParticleController
     {
+        Texture2D _particleTexture;
+        Vector2 _cameraPosition;
+        List<Particle> Cursor = new List<Particle>();
 
+        float _timer = 0;
+
+        public ParticleController(Texture2D particleTexture)
+        {
+            _particleTexture = particleTexture;
+        }
+
+        public void Update(GameTime gameTime, Vector2 cameraPosition)
+        { 
+            _cameraPosition = cameraPosition;
+
+            _timer -= (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+            if (_timer < 0)
+            {
+                _timer += 10;
+                Cursor.Add(new Particle(_particleTexture));
+            }
+        }
+
+        public void Draw()
+        {
+        }
     }
 }
